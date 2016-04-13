9f84af2f6843c8fdb7b3ff162149fd365e3e9e28
John Anthony Rivera <askinganthony@gmail.com>
2016-04-12 21:41:07 -0700

    Progress checkpoint - just started critique-form directive

---
+++ app/assets/javascripts/front_end/directives/mix/critique/critiqueForm.js
@@ -0,0 +1,31 @@
+function critiqueForm() {
+	return {
+		restrict: 'E',
+		scope: {
+			revision: '='
+		},
+		controller: function(ApiService, $sce){
+			var ctrl =  this;
+			ctrl.show = false;
+
+			ctrl.load = function(id){
+				ApiService.getRevision(rev).then(function(res){
+					ctrl.revision = res.data;
+					ctrl.show = true;
+				});
+
+			};
+  	},
+		controllerAs: 'RevCtrl',
+		templateUrl: 'mix/critique/critique_form_template.html',
+		link: function(scope, elem, attrs, ctrl) {
+			scope.$watch('revision', function (rev) {
+	        if (rev!==undefined) {ctrl.load(rev); console.log(rev); }
+	    });
+		}
+	};
+}
+
+angular
+	.module('app')
+	.directive('critiqueForm', critiqueForm);
