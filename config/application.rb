8ebd9c99992514cf209bad43a59fa1afe16c5da5
John Anthony Rivera <askinganthony@gmail.com>
2016-05-03 17:58:05 -0700

    Now looks to env for secret key base

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MixCritic
   class Application < Rails::Application
 
-    
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     config.active_record.raise_in_transactional_callbacks = true
 
