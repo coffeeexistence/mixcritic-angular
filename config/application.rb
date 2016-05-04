ae14e751db0affe9be10b2d45008abca2feeebd5
John Anthony Rivera <askinganthony@gmail.com>
2016-05-03 17:27:03 -0700

    Ready to be deployed

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module MixCritic
   class Application < Rails::Application
 
+    
+
     config.active_record.raise_in_transactional_callbacks = true
 
     AWS.config(
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,7 +262,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
   config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"], callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
 
