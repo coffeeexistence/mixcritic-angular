892df41156d5b1a037e04aa09770e1f6b512c86b
John Anthony Rivera <askinganthony@gmail.com>
2016-06-06 15:10:10 -0700

    Fixed default image path issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   has_many :critique_comments
 
-  has_attached_file :avatar, default_url: ':style/default.png', :styles => {
+  has_attached_file :avatar, default_url: '/user_avatars/:style/default.png', :styles => {
       :tiny => "50x50#",
       :thumb => "100x100#",
       :small  => "150x150>",
@@ -44,14 +44,27 @@ class User < ActiveRecord::Base
     return false if (!self.first_name || !self.last_name)
     self.first_name + ' ' + self.last_name
   end
+  
+  def image_url(image)
+    (image = image[1..-1]) if (image[0] == '/')
+    ActionController::Base.helpers.asset_path(image)
+  end
+  
+  def has_image?
+    self.avatar.file?
+  end
 
   def image_paths
-    {
-      tiny: self.avatar.url(:tiny),
-      thumb: self.avatar.url(:thumb),
-      small: self.avatar.url(:small),
-      medium: self.avatar.url(:medium)
-    }
+    tiny = self.avatar.url(:tiny)
+    thumb = self.avatar.url(:thumb)
+    small = self.avatar.url(:small)
+    medium = self.avatar.url(:medium)
+        
+    if self.has_image?
+      { tiny: tiny, thumb: thumb, small: small, medium: medium }
+     else 
+      { tiny: self.image_url(tiny), thumb: self.image_url(thumb), small: self.image_url(small), medium: self.image_url(medium) }
+     end
   end
 
   def critiques_index
