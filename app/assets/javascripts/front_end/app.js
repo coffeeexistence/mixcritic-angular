eaaebae49d4bf39a7fcbc42f274d7c6bd5ad1b66
John Anthony Rivera <askinganthony@gmail.com>
2016-06-28 15:56:26 -0700

    Top navbar working, basic color scheme set up

--- app/assets/javascripts/front_end/app.js
+++ app/assets/javascripts/front_end/app.js
@@ -10,8 +10,46 @@ angular
      'angularUtils.directives.dirPagination', 
      'ng-resource-manager'
      ])
-      .config(['$stateProvider', '$urlMatcherFactoryProvider', '$urlRouterProvider',
-      function($stateProvider, $urlMatcherFactoryProvider, $urlRouterProvider){
+      .config(['$stateProvider', '$urlMatcherFactoryProvider', '$urlRouterProvider', '$mdThemingProvider',
+      function($stateProvider, $urlMatcherFactoryProvider, $urlRouterProvider, $mdThemingProvider){
+        
+        
+        $mdThemingProvider.definePalette('min', {
+          '50':  '050505',
+          '100': '2B2B2B',
+          
+          '200': '2B2B2B',
+          '300': '2B2B2B',
+          
+          '400': '050505',
+          '500': 'FAFAFA',
+          
+          '600': 'FAFAFA',
+          '700': 'D3D3D3',
+          
+          '800': 'C6C6C6',
+          '900': 'B7B7B7',
+          
+          'A100': '2B2B2B',
+          'A200': 'FAFAFA',
+          'A400': 'FAFAFA',
+          'A700': 'd5d5d5',
+          'contrastDefaultColor': 'dark',    // whether, by default, text (contrast)
+                                              // on this palette should be dark or light
+          'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+          '200', '300', '400', 'A100'],
+          'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        });
+        
+        $mdThemingProvider.theme('default')
+          .primaryPalette('min', {
+            'default': '400', // by default use shade 400 from the pink palette for primary intentions
+            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+          })
+          .accentPalette('blue-grey');
+        
         $urlMatcherFactoryProvider.strictMode(false);
         $urlRouterProvider.otherwise('');
         $stateProvider
